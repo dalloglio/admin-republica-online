5eb1094c6e6bb00e994cb19b19f52f118194ff55
dalloglio <ricardo.tech@live.com>
2017-07-17 13:45:56 -0300

    Nav menu.

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,57 +1,16 @@
 <template>
   <div class="hello">
-    <h3>RESPONSIVE</h3>
-    <el-row :gutter="5">
-      <el-col :xs="8" :sm="6" :md="4" :lg="3"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :xs="4" :sm="6" :md="8" :lg="9"><div class="grid-content bg-purple-light"></div></el-col>
-      <el-col :xs="4" :sm="6" :md="8" :lg="9"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :xs="8" :sm="6" :md="4" :lg="3"><div class="grid-content bg-purple-light"></div></el-col>
-    </el-row>
 
-    <h3>FLEX</h3>
-    <el-row :gutter="5" type="flex" justify="space-around">
-      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-    </el-row>
 
-    <h3>FLEX VERTICAL</h3>
-    <el-row :gutter="5" type="flex" justify="center" align="middle" style="height: 300px; background-color: #000">
-      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-    </el-row>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Element.io'
-    }
-  }
+  name: 'hello'
 }
 </script>
 
 <style scoped>
-  h3 {
-    text-align: center;
-    border-bottom: 1px solid #ccc;
-  }
-  .el-col {
-    margin-bottom: 5px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    min-height: 36px;
-  }
 </style>

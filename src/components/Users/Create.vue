a76b81cf081e79aae01907d6a5df88fe08341a70
dalloglio <ricardo.tech@live.com>
2017-07-20 08:22:31 -0300

    Component create user.

--- src/components/Users/Create.vue
+++ src/components/Users/Create.vue
@@ -1,14 +1,49 @@
-<template lang="html">
+<template>
   <div class="users create">
-    <h1>Create</h1>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ name: 'home' }">Home</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ name: 'users.index' }">Usuários</el-breadcrumb-item>
+      <el-breadcrumb-item>Criar</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <h1>
+      Criar usuário
+      <el-button-group class="right">
+        <el-button type="primary">Voltar</el-button>
+      </el-button-group>
+    </h1>
+
+    <el-card class="box-card">
+      <el-form label-position="top" :model="form">
+        <el-form-item label="Name">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="E-mail">
+          <el-input v-model="form.email"></el-input>
+        </el-form-item>
+        <el-form-item label="Senha">
+          <el-input v-model="form.password"></el-input>
+        </el-form-item>
+        <el-button type="success">Salvar</el-button>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'users-create'
+  'name': 'users-create',
+  data () {
+    return {
+      form: {
+        name: '',
+        email: '',
+        password: ''
+      }
+    }
+  }
 }
 </script>
 
-<style lang="css">
+<style>
 </style>

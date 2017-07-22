5867a9b88232b0ee9fdf24ecfd5ed39f78632230
Ricardo Pires Dall Oglio <ricardo.tech@live.com>
2017-07-22 10:58:24 -0300

    Configurado momentjs e plugin para converter datas.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/pt-br'
 import 'element-ui/lib/theme-default/index.css'
 import 'font-awesome/css/font-awesome.css'
 
 import App from './App'
 import router from './router'
 import { store } from './store'
-import Url from './utils/url'
+import UtilsUrl from './utils/url'
+import UtilsDate from './utils/date'
 
 Vue.use(VueResource)
-Vue.use(Url)
-Vue.use(ElementUI)
+Vue.use(ElementUI, { locale })
+Vue.use(UtilsUrl)
+Vue.use(UtilsDate)
 
 Vue.http.options.root = process.env.API_URL
 // window.axios.defaults.baseURL = process.env.API_URL

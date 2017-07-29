2c2578fc19bf36e3790a4a1f925c9878fdaa3f23
Ricardo Pires Dall Oglio <ricardo.tech@live.com>
2017-07-28 22:43:05 -0300

    Corrigido url da api para o ambiente de produção.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"http://api.republica.online/v1"',
+  API_URL: '"http://api.republica.online"',
   API_CLIENT_ID: '"2"',
   API_CLIENT_SECRET: '"q9S5DdzE8WRk7ZycKNFQZEB30WsxaPPLvX4v6rbU"',
   API_VERSION: '"v1"'

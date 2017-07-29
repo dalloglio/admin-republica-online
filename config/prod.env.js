4429ca45823517c63d3cb29534f7121d2de022bf
Ricardo Pires Dall Oglio <ricardo.tech@live.com>
2017-07-28 21:44:54 -0300

    Adicionar variaveis para ambiente de produção

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,6 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_URL: '"http://api.republica.online/v1"',
+  API_CLIENT_ID: '"2"',
+  API_CLIENT_SECRET: '"q9S5DdzE8WRk7ZycKNFQZEB30WsxaPPLvX4v6rbU"'
 }
